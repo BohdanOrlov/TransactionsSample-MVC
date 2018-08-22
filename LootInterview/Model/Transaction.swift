f15b7f91980e88c96e2daa47b0ca4b6b899bd985
Tom Angistalis <tom.angistalis@gmail.com>
2018-08-22 13:56:27 +0300

    Updated model to use correct Codable implementation.

--- LootInterview/Model/Transaction.swift
+++ LootInterview/Model/Transaction.swift
@@ -7,19 +7,60 @@
 //
 
 import Foundation
+import MapKit
 
 struct Location: Codable, Equatable {
-    var latitude: String
-    var longitude: String
+    var latitude: Double?
+    var longitude: Double?
+    
+    init(from decoder: Decoder) throws {
+        let map = try decoder.container(keyedBy: CodingKeys.self)
+        if let latitudeString = try? map.decode(String.self, forKey: .latitude) {
+            latitude = Double(latitudeString) ?? 0
+        }
+        if let longitudeString = try? map.decode(String.self, forKey: .longitude) {
+            longitude = Double(longitudeString) ?? 0
+        }
+    }
 }
 
 struct Transaction: Codable, Equatable {
     
-    var amount: String
+    var amount: Int
     var description: String
-    var postTransactionBalance: String
+    var postTransactionBalance: Int
     var settlementDate: Date
     var authorisationDate: Date
     
     var location: Location?
+    
+    init(from decoder: Decoder) throws {
+        let map = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let amountString = try? map.decode(String.self, forKey: .amount) {
+            amount = Int(amountString) ?? 0
+        } else {
+            amount = 0
+        }
+        
+        if let postTransactionBalanceString = try? map.decode(String.self, forKey: .postTransactionBalance) {
+            postTransactionBalance = Int(postTransactionBalanceString) ?? 0
+        } else {
+            postTransactionBalance = 0
+        }
+        
+        description = try map.decode(String.self, forKey: .description)
+        settlementDate = try map.decode(Date.self, forKey: .settlementDate)
+        authorisationDate = try map.decode(Date.self, forKey: .authorisationDate)
+        location = try map.decodeIfPresent(Location.self, forKey: .location)
+    }
+}
+
+extension Transaction {
+    var coordinates: CLLocationCoordinate2D? {
+        guard let latitude = location?.latitude else { return nil }
+        guard let longitude = location?.longitude else { return nil }
+        
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
 }
